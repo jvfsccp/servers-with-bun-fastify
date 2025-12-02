1b77954ad15ca883abfbf709adba6b80a2d8111a
João Victor <joaovictorfernandescastro@gmail.com>
2025-12-02 14:01:43 -0300

    sec-2 iniciada + roteamento no fastify

---
+++ sec-2/lec-6-Routes/shorthand/start/shorthand/src/index.ts
@@ -0,0 +1,39 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get("/", async (req, rep) => {
+  return "hello world"
+})
+
+// server.get<{
+//   Headers: {myheader: number}
+//   Querystring: { id: number}
+//   Reply: {
+//     200: { status: string },
+//     500: { code: number }
+//   }
+// }>("/employee", async (req, rep) => {
+//   // return req.headers.myheader
+//   // return `you passed ${req.query.id}`
+
+//   return rep.status(200).send({ status: "success" })
+// })
+
+server.post<{
+  Body: { userName: string}
+}>("/employee", async (req, rep) => {
+   return `userName is ${req.body.userName}`
+})
+
+server.get<{ Params: { id: string }}>("/employee/:id", async (req, rep) => {
+  return `id is ${req.params.id}`
+})
+
+server.listen({port: 8080, host: "::1"}, (err, address) => {
+  if (err) {
+    console.log("err", err)
+    process.exit(1)
+  }
+  console.log(`server is a ready on ${address}`)
+})
